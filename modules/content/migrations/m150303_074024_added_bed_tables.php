d7e8a11732a1821b35e8441b8de3e00b3279aed7
Archy <archy@forrader.ru>
2015-03-04 06:46:52 +0300

    fixed block table fields

--- modules/content/migrations/m150303_074024_added_bed_tables.php
+++ modules/content/migrations/m150303_074024_added_bed_tables.php
@@ -32,13 +32,13 @@ class m150303_074024_added_bed_tables extends Migration
         $this->createTable($this->blockTbl, [
             'id' => 'serial primary key',
             'id_bed' => 'int not null',
-            'is_enabled' => 'boolean not null default true',
+            'is_active' => 'boolean not null default true',
             'position' => 'smallint not null',
-            'widget_class' => 'varchar(255) not null',
+            'source' => 'varchar(255) not null',
             'params' => 'jsonb',
         ]);
 
-        $this->createIndex($this->blockTbl . '_is_enabled', $this->blockTbl, 'is_enabled');
+        $this->createIndex($this->blockTbl . '_is_active', $this->blockTbl, 'is_active');
         $this->createIndex($this->blockTbl . '_position', $this->blockTbl, 'position');
 
         $this->addForeignKeyWithAutoNamed($this->bedTbl, 'id_proto', $this->bedTbl, 'id');
@@ -50,7 +50,7 @@ class m150303_074024_added_bed_tables extends Migration
         $this->dropForeignKey($this->generateFkName($this->bedTbl, 'id_proto', $this->bedTbl, 'id'), $this->bedTbl);
         $this->dropForeignKey($this->generateFkName($this->blockTbl, 'id_bed', $this->bedTbl, 'id'), $this->blockTbl);
 
-        $this->dropIndex($this->blockTbl . '_is_enabled', $this->blockTbl);
+        $this->dropIndex($this->blockTbl . '_is_active', $this->blockTbl);
         $this->dropIndex($this->blockTbl . '_position', $this->blockTbl);
 
         $this->dropTable($this->blockTbl);
